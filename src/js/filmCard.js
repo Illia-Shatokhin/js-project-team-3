6acbe9725a56fabe30d707cf259423d44e97a34a
KaterinaKha <katerinakhaa@gmail.com>
2023-06-02 22:21:46 +0200

    markUp

--- src/js/filmCard.js
+++ src/js/filmCard.js
@@ -1,25 +1,5 @@
 import axios from 'axios';
-
-function createMovieCardMarkup({ data }) {
-  console.log(data);
-  let releaseYear = 'No data available';
-  if (!!data.release_date) {
-    releaseYear = data.release_date.split('-')[0];
-  }
-  return `    
-  <li class="catalog-item" id="${data.results.id}">
-  <img class="catalog-card-img" href="${data.results.backdrop_path}"></img>
-
-  <div class="catalog-card-info-container">
-    <h4 class="catalog-card-title">${data.results.title}</h4>
-    <pcatalog-card-description><span class="card-info-span">${data.results.genre_ids}</span> | <span
-        class="card-info-span">${releaseYear}</span>
-    </p>
-    <div class="rating">${data}</div>
-  </div>
-</li>
-`;
-}
+import createMovieCardMarkup from './movieCardMarkup';
 
 function getWeeklyTrands() {
   const options = {
@@ -36,11 +16,8 @@ function getWeeklyTrands() {
   axios
     .request(options)
     .then(function (response) {
-      console.log(response.data);
-      response.data.results.map(el => {
-        console.log(el);
-        createMovieCardMarkup(el);
-      });
+      console.log(response);
+      createMovieCardMarkup(response, catalogCard);
     })
     .catch(function (error) {
       console.error(error);
@@ -48,3 +25,6 @@ function getWeeklyTrands() {
 }
 
 getWeeklyTrands();
+
+const catalogCard = document.querySelector('.catalog-list');
+export { catalogCard };
