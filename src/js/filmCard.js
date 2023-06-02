c6f8cbcd0f9165bc3bb628fcf2fa6e0fe9d65047
KaterinaKha <katerinakhaa@gmail.com>
2023-06-02 15:11:44 +0200

    add api

---
+++ src/js/filmCard.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const API_KEY_harkova = '6da8758eb40e221013709d63079fd9c4';
+const URL_PREFIX = 'https://api.themoviedb.org/3/search/movie';
+
+async function getMoviesFromSearch(value){
+  // https://api.themoviedb.org/3/search/movie?query=value&include_adult=false&language=en-US&page=1
+  
+}
+
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     'content-type': 'application/json',
+//     Authorization:
+//       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGE4NzU4ZWI0MGUyMjEwMTM3MDlkNjMwNzlmZDljNCIsInN1YiI6IjY0NzlhODg5ZTMyM2YzMDBhN2Q0ZGYzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.X0uZkuPOvO6LdN-fYieV72t0jFMKCqqFgnPHTxpIpdg',
+//   },
+// };
+
+// fetch(
+//   'https://api.themoviedb.org/3/search/movie?query=1941&include_adult=false&language=en-US&',
+//   options
+// )
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+
+
+
+
+
+console.log('hello world');
+
+function createMarkupFilmCard()
