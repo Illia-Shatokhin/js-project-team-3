dc466070974410f777f04cbb464a892c824607ea
KaterinaKha <katerinakhaa@gmail.com>
2023-06-03 21:50:53 +0200

    css done

--- src/js/markups/movieCardMaurkup.js
+++ src/js/markups/movieCardMaurkup.js
@@ -1,12 +1,12 @@
 export function movieCardMarkup(card, releaseYear) {
   return `
-<li class="catalog-item" style="background-size: cover;background-image: linear-gradient(180deg, rgba(0, 0, 0, 0) 63.48%, rgba(0, 0, 0, 0.9) 92.16%), url(https://image.tmdb.org/t/p/w500${
+<li class="catalog-item" style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0) 63.48%, rgba(0, 0, 0, 0.9) 92.16%), url(https://image.tmdb.org/t/p/w500${
     card.poster_path
   })" id="${card.id}">
 
 <div class="catalog-card-info-container">
-  
-  <h4 class="catalog-card-title">${card.original_title}</h4>
+    <h4 class="catalog-card-title">${card.original_title}</h4>
+
   <div class="film-info-container">
     <p class="catalog-card-description">${card.genre_ids} | ${releaseYear}
     </p>
