a9634b8659c23c32cb885adb3cbe1ab96d93ad72
Illia Shatokhin <resonane.2106@gmail.com>
2023-06-03 14:04:14 +0300

    comment main js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import '/js/hero.js';
 
-import createCatalogMovieCard from './js/catalogMovieCard';
-import {
-  getGenreMovieList,
-  getTrendingAllWeek,
-} from './js/API/get-from-server';
+// import createCatalogMovieCard from './js/catalogMovieCard';
+// import {
+//   getGenreMovieList,
+//   getTrendingAllWeek,
+// } from './js/API/get-from-server';
 
-const catalogList = document.querySelector('.catalog-list');
-const weeklyTrendsData = await getTrendingAllWeek();
-createCatalogMovieCard(weeklyTrendsData, catalogList);
+// const catalogList = document.querySelector('.catalog-list');
+// const weeklyTrendsData = await getTrendingAllWeek();
+// createCatalogMovieCard(weeklyTrendsData, catalogList);
