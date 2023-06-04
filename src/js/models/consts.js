cf082ad0547bb233a69eb7b70df9825195414ab2
Serhii Podhornyi <serhii.podhornyi@gmail.com>
2023-06-04 23:43:25 +0300

    fixed url

--- src/js/models/consts.js
+++ src/js/models/consts.js
@@ -1,6 +1,6 @@
 export const CONSTS = {
   API_KEY: 'ec7594db8ba2de756a869df7f4a7ac80',
-  URL: 'https://api.themoviedb.org/31',
+  URL: 'https://api.themoviedb.org/3',
   BEARER:
     'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNzgzN2Q4ZThiOWY1YjkyODFlNGYzODM2ZjQwZmMzMiIsInN1YiI6IjY0NzhmMTllMGUyOWEyMDBkY2I5YmFkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Gm8FRVhZa5JYfHHhkK7gHuf4DwF_mvLWBXC6uzMdhLk',
 };
