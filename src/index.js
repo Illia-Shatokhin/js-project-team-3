1c2ec54694743fbfd3a56b3a4497f333b896b4f0
Illia Shatokhin <resonane.2106@gmail.com>
2023-06-04 19:50:07 +0300

    debug

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@
 // createHero();
 
 import '/js/hero.js';
-import '/js/upcoming-films.js'
+import '/js/upcoming-films.js';
+function renderStars(rating) {
+  const integer = Math.round(rating);
+  const even = integer % 2 === 0 ? integer : integer - 1;
+  const diff = Math.round(10 - rating);
+  console.log(diff);
+}
+renderStars(7.4);
