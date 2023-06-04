8560cb8428a7595e5419ef8164f6579c22c4864d
Gabriella Marusyak <slipuhinag@gmail.com>
2023-06-04 21:26:26 +0300

    add remove from localStoradge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import '/js/hero.js';
 import '/js/modalWindow.js';
-import { getMovie} from '/js/modalWindow.js';
- document.querySelector('body').addEventListener('click', ()=>{
-    getMovie( 605579);
- })
+// import { getMovie} from '/js/modalWindow.js';
+//  document.querySelector('body').addEventListener('click', ()=>{
+//     getMovie( 605579);
+//  })
