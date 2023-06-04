b5d362930abe60baf1d0d5079cb33b49ffdac99e
Illia Shatokhin <resonane.2106@gmail.com>
2023-06-04 19:57:00 +0300

    debug

--- src/index.js
+++ src/index.js
@@ -10,10 +10,3 @@ import { getMovie } from '/js/modalWindow.js';
 //     getMovie( 605575);
 // })
 import '/js/upcoming-films.js';
-function renderStars(rating) {
-  const integer = Math.round(rating);
-  const even = integer % 2 === 0 ? integer : integer - 1;
-  const diff = Math.round(10 - rating);
-  console.log(diff);
-}
-renderStars(7.4);
