02943cfa5204721042a5cde7eed7be3e86216799
mandarinca <myler4ik@gmail.com>
2023-06-05 18:24:51 +0300

    Pull all changes after push

--- src/js/models/data-for-library.js
+++ src/js/models/data-for-library.js
@@ -2,10 +2,13 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 import { getMovieDetails } from '../API/get-from-server';
 // import createCatalogMovieCard from './../catalogMovieCard';
+import { errorLibraryMarkup } from "../../js/errortrailer.js";
+import { CONSTS } from './consts';
 
 
 
 const filmsOfLocalStorage = document.querySelector('.my-library-films');
+
 filmsOfLocalStorage.style.cssText =
   ` display: flex;
     flex-flow: row wrap;
@@ -14,12 +17,10 @@ filmsOfLocalStorage.style.cssText =
   `;
 
 const libraryIdsArr = localStorage.getItem('my_fake_library') ? JSON.parse(localStorage.getItem('my_fake_library')) : [];
-console.log('This Without parse:', localStorage.getItem('my_fake_library'));
 
 function addtoLocalStorage(num) {
   if (!num) return
   const test = JSON.parse(localStorage.getItem('my_fake_library'));
-
   if (!test.includes(num)) {
     test.push(num);
     localStorage.setItem('my_fake_library', JSON.stringify(test))
@@ -38,40 +39,28 @@ function removeLibraryIdsArr(id) {
     localStorage.setItem("my_fake_library", JSON.stringify(getDatasLocalStorage));
   }
 }
-
 removeLibraryIdsArr(569094);
 
-
 const libraryArr = [];
-
-console.log('lol', libraryIdsArr);
-
 
 async function getFromLocalStorage() {
   for (let i = 0; i < libraryIdsArr.length; i++) {
     const el = libraryIdsArr[i];
-    console.log(el);
+
     try {
       const resp = await getMovieDetails(el)
-      console.log("🚀 ~ file: data-for-library.js:19 ~ getFromLocalStorage ~ resp:", resp)
-
-      // createCatalogMovieCard(getMovieDetails, filmsOfLocalStorage, resp.id);
-
       libraryArr.push(resp.data);
     } catch (err) {
-
-      console.error(err);
       Notiflix.Report.warning('Error', 'Not working');
     }
   };
 
   if (libraryArr.length == 0) {
-    //Here must be Dimas function "OOOps....."
-    console.log('No libraries found')
+    errorLibraryMarkup();
   } else {
     console.log('lol');
   }
 }
-getFromLocalStorage()
+
 
 
