069b68ee4f5c71049c72e1ff150a0ad44ef22cd0
Serhii Podhornyi <serhii.podhornyi@gmail.com>
2023-06-05 03:11:23 +0300

    05_06 3:11

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,4 +1,5 @@
 import { getTrendingAllDay } from './API/get-from-server.js';
+// import { getTrailer } from '.hero-trailer.js';
 
 const heroRef = document.querySelector('.hero');
 
@@ -9,20 +10,6 @@ const gradient768 =
 const gradient1280 =
   'linear-gradient(83.06deg, #111111 11.91%, rgba(17, 17, 17, 0) 73.11%)';
 
-// const gradient320HomeStub =
-//   'linear-gradient(86.77deg, #111111 30.38%, rgba(17, 17, 17, 0) 65.61%)';
-// const gradient768HomeStub =
-//   'linear-gradient(82.55deg, #111111 39.6%, rgba(17, 17, 17, 0) 72.95%)';
-// const gradient1280HomeStub =
-//   'linear-gradient(83.16deg, #111111 36.85%, rgba(17, 17, 17, 0) 60.05%)';
-
-// const gradient320LibStub =
-//   'linear-gradient(79.49deg, #111111 34.1%, rgba(17, 17, 17, 0) 64.67%)';
-// const gradient768LibStub =
-//   'linear-gradient(77.77deg, #111111 33.58%, rgba(17, 17, 17, 0) 71.57%)';
-// const gradient1280LibStub =
-//   'linear-gradient(79.39deg, #111111 32.37%, rgba(17, 17, 17, 0) 72.02%)';
-
 export function createHero() {
   getDataHero();
 }
@@ -31,42 +18,65 @@ async function getDataHero() {
   try {
     const data = await getTrendingAllDay();
     renderHero(data.results);
+    // !------------------
+    // console.log('Ламаю запит');
+    // !--------------
   } catch (error) {
     error => console.log(error);
   }
+
+  // * Це ще в експеременті------------------
+  const watchTrailerBtn = document.getElementById('watch-trailer-btn');
+  watchTrailerBtn.addEventListener('click', e => {
+    console.dir(e.currentTarget);
+    // getTrailer();
+  });
+  // *---------------------------
 }
 
 function renderHero(data) {
   const index = getRandomIndex();
 
-  const { id, overview, original_title, vote_average, backdrop_path } =
-    data[index];
+  const { id, overview, title, vote_average, backdrop_path } = data[index];
 
   heroRef.setAttribute('id', `${id}`);
 
+  // ?---------------------тест заглушки----------------
+  // addHeroBackgroundStub();
+  // heroRef.innerHTML = creatHeroMarkupStub(overview, title, vote_average);
+  // ?----------------------------------
+
   if (!data) {
     addHeroBackgroundStub();
-    heroRef.innerHTML = creatHeroMarkupStub(
-      overview,
-      original_title,
-      vote_average
-    );
+    heroRef.innerHTML = creatHeroMarkupStub(overview, title, vote_average);
   } else {
+    // !----------Перевірка на погану карточку - якщо не всі дані перезапускаємо функцію знову, шукаємо іншу-------
+    if (
+      id === undefined ||
+      title === undefined ||
+      overview === undefined ||
+      vote_average === undefined ||
+      backdrop_path === undefined
+    ) {
+      return createHero();
+    }
+    // !-----------------------------------------------------------------
+
     addHeroBackground(backdrop_path);
-    heroRef.innerHTML = creatHeroMarkup(overview, original_title, vote_average);
+    heroRef.innerHTML = creatHeroMarkup(overview, title, vote_average);
   }
 }
 
-function creatHeroMarkup(overview, original_title, vote_average) {
+function creatHeroMarkup(overview, title, vote_average) {
   return `
     <div class="container hero-container">
-      <h2 class="hero-title">${original_title}</h2>
+      <h2 class="hero-title">${title}</h2>
       <p class="reting-stars">${vote_average.toFixed(1)}</p>
       <div class="overview">
         <p class="overview-text">${overview}</p>
       </div>
       <div class="thumb-hero-btn">
-        <button class="button btn-gradient hero-btn">Watch trailer</button>
+        <button class="button btn-gradient hero-btn" id="watch-trailer-btn">Watch trailer</button>
         <button class="button btn-transparent-dark hero-btn">More details</button>
       </div>
     </div>`;
@@ -77,48 +87,50 @@ function getRandomIndex() {
 }
 
 function addHeroBackground(backdrop_path) {
-  if (window.innerWidth <= 767) {
+  if (document.documentElement.clientWidth <= 767) {
     heroRef.style.backgroundImage = `${gradient320},
     url(https://www.themoviedb.org/t/p/original/${backdrop_path})`;
   }
 
-  if (window.innerWidth >= 768 && window.innerWidth <= 1279) {
+  if (
+    document.documentElement.clientWidth >= 768 &&
+    window.innerWidth <= 1279
+  ) {
     heroRef.style.backgroundImage = `${gradient768},
     url(https://www.themoviedb.org/t/p/original/${backdrop_path})`;
   }
 
-  if (window.innerWidth >= 1280) {
+  if (document.documentElement.clientWidth >= 1280) {
     heroRef.style.backgroundImage = `${gradient1280},
     url(https://www.themoviedb.org/t/p/original/${backdrop_path})`;
   }
 }
-//! Перевірити адресу картинки
+
 function addHeroBackgroundStub() {
-  if (window.innerWidth <= 320) {
-    heroRef.style.backgroundImage = `${gradient320HomeStub},
-    url(./public/img/hero.mob.jpeg})`;
-  }
-
-  if (window.innerWidth >= 321 && window.innerWidth <= 768) {
-    heroRef.style.backgroundImage = `${gradient768},
-    url(https://www.themoviedb.org/t/p/original/${backdrop_path})`;
-  }
-
-  if (window.innerWidth >= 769) {
-    heroRef.style.backgroundImage = `${gradient1280},
-    url(https://www.themoviedb.org/t/p/original/${backdrop_path})`;
-  }
+  heroRef.classList.add('hero-stub');
 }
 
 function creatHeroMarkupStub() {
+  let overviewTextStub = '';
+
+  if (document.documentElement.clientWidth <= 767) {
+    overviewTextStub =
+      "Is a guide to creating a personalized movie theater experience. You'll need a projector, screen, and speakers.";
+  }
+
+  if (document.documentElement.clientWidth >= 768) {
+    overviewTextStub =
+      "Is a guide to creating a personalized movie theater experience. You'll need a projector, screen, and speakers. Decorate your space, choose your films, and stock up on snacks for the full experience.";
+  }
+
   return `
     <div class="container hero-container">
-      <h2 class="hero-title">Let’s Make Your Own Cinema</h2>
+      <h2 class="hero-title hero-title-stub">Let’s Make Your Own Cinema</h2>
       <div class="overview">
-        <p overview-text>Is a guide to creating a personalized movie theater experience. You'll need a projector, screen, and speakers. Decorate your space, choose your films, and stock up on snacks for the full experience.</p>
+        <p class="overview-text-stub">${overviewTextStub}</p>
       </div>
       <div class="thumb-hero-btn">
-        <button class="button btn-gradient hero-btn">Get Started</button>
+        <a href="./my-library.html" class="button btn-gradient hero-btn">Get Started</a>
       </div>
     </div>`;
 }
