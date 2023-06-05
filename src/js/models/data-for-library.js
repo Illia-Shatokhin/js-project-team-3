798ed194aea49504d5954d88720b9a53652f41ca
mandarinca <myler4ik@gmail.com>
2023-06-05 22:21:19 +0300

    New Local Storage

--- src/js/models/data-for-library.js
+++ src/js/models/data-for-library.js
@@ -6,6 +6,7 @@ import { errorLibraryMarkup, renderError } from './../errortrailer';
 
 
 //add to css
+
 const filmsOfLocalStorage = document.querySelector('.my-library-list');
 filmsOfLocalStorage.style.cssText =
   ` display: flex;  
@@ -14,68 +15,13 @@ filmsOfLocalStorage.style.cssText =
     gap: 15px;
   `;
 
-const libraryIdsArr = localStorage.getItem('my_fake_library')
-  ? JSON.parse(localStorage.getItem('my_fake_library'))
-  : [];
+const libraryFromLocal = localStorage.getItem('myLibrary') ? JSON.parse(localStorage.getItem('myLibrary')) : []
 
 
-function addtoLocalStorage(num) {
-  if (!num) return;
-  const test = JSON.parse(localStorage.getItem('my_fake_library'));
-  if (!test.includes(num)) {
-    test.push(num);
-    localStorage.setItem('my_fake_library', JSON.stringify(test));
-  }
+
+if (libraryFromLocal.length == 0) {
+  renderError(filmsOfLocalStorage, errorLibraryMarkup)
+} else {
+  createMovieCard(libraryFromLocal, filmsOfLocalStorage, libraryFromLocal.length < 9 ? libraryFromLocal.length : 9);
+  //if libraryFromLocal.length > 9 create load more btn...
 }
-addtoLocalStorage(569097);
-
-// recive item.id from btn-remove
-function removeLibraryIdsArr(id) {
-  if (!id) return;
-  const getDatasLocalStorage = JSON.parse(
-    localStorage.getItem('my_fake_library')
-  );
-  const index = getDatasLocalStorage.indexOf(id);
-  if (index !== -1) {
-    getDatasLocalStorage.splice(index, 1);
-    localStorage.setItem(
-      'my_fake_library',
-      JSON.stringify(getDatasLocalStorage)
-    );
-  }
-}
-removeLibraryIdsArr(569094);
-
-const libraryArr = [];
-
-console.log('lol', libraryIdsArr);
-
-async function getFromLocalStorage() {
-  for (let i = 0; i < libraryIdsArr.length; i++) {
-    const el = libraryIdsArr[i];
-
-    try {
-      const resp = await getMovieDetails(el);
-
-      libraryArr.push(resp);
-
-      // createMovieCard(libraryArr, filmsOfLocalStorage, 9);
-
-      // createCatalogMovieCard(getMovieDetails, filmsOfLocalStorage, resp.id);
-
-      // libraryArr.push(resp.data);
-    } catch (err) {
-      console.error(err);
-      Notiflix.Report.warning('Error', 'Not working');
-    }
-  }
-
-
-
-  if (libraryArr.length == 0) {
-    renderError(filmsOfLocalStorage, errorLibraryMarkup)
-  } else {
-    createMovieCard(libraryArr, filmsOfLocalStorage, 9);
-  }
-}
-getFromLocalStorage();
