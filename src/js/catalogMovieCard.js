30c4653f03c014cef258292c13eb4259867eb5dd
KaterinaKha <katerinakhaa@gmail.com>
2023-06-07 00:35:12 +0200

    css, connect stars

--- src/js/catalogMovieCard.js
+++ src/js/catalogMovieCard.js
@@ -5,6 +5,7 @@ import { refs } from './models/refs';
 import { getGenreMovieList, getTrendingAllWeek } from './API/get-from-server';
 import CreatePagination from './services/pagination';
 
+const ratingArray = [];
 //================================================================
 function getReleaseYear(film) {
   let releaseYear = '2023';
@@ -12,13 +13,6 @@ function getReleaseYear(film) {
   if (release_date) releaseYear = release_date.split('-')[0];
   return releaseYear;
 }
-
-// function getMovieTitle(film) {
-//   let originalTitle = 0;
-//   const { original_title } = film;
-//   if (original_title) originalTitle = original_title;
-//   return originalTitle;
-// }
 
 function getMovieTitle(film) {
   let originalTitle = 'No Title';
@@ -38,12 +32,18 @@ export default async function createMovieCard(data, elem, count) {
   const genreIds = await getGenreIds(getGenreMovieList);
   let markup = '';
   for (let index = 0; index < count; index++) {
+    ratingArray.push(data[index].vote_average ? data[index].vote_average : 0);
+
     const releaseYear = getReleaseYear(data[index]);
     const originalTitle = getMovieTitle(data[index]);
     const movieGenres = genreIds.filter(genre =>
       data[index].genre_ids.includes(genre.id)
     );
-    const genreNames = movieGenres.map(genre => genre.name).join(', ');
+    const genreNames = movieGenres
+      .map(genre => genre.name)
+      .slice(0, 2)
+      .join(', ');
+
     if (data)
       markup += movieCardMarkup(
         data[index],
@@ -53,6 +53,27 @@ export default async function createMovieCard(data, elem, count) {
       );
   }
   elem.innerHTML = markup;
+  addStars([...document.querySelectorAll('.catalog-card-rating')]);
+}
+
+//================================================================
+import ReviewStars from './stars';
+
+export function addStars(data) {
+  data.map(el => {
+    const stars = new ReviewStars({
+      totalStars: 5,
+      averageRating: ratingArray.shift() / 2,
+      size: 14,
+      filled: false,
+      insertTo: {
+        element: el,
+        position: 'afterbegin',
+      },
+    });
+    document.addEventListener('click', () => stars.restart());
+    setTimeout(() => stars.restart(), 1000);
+  });
 }
 
 //================================================================
@@ -65,6 +86,7 @@ export async function weeklyTrendsList(page = 1) {
 
     const watchedPagination = new CreatePagination(data, weeklyTrendsList);
     watchedPagination.activatePagination();
+    addStars();
   } catch (error) {
     renderError(refs.catalogList, errorCatalogMarkup);
   }
