2bc55031e24aed3cfd20e18e4e9d73cb9431fab4
Nadiia Samborska <dilova_3@ukr.net>
2023-06-06 22:18:12 +0300

    update pagination

--- src/js/catalogSearch.js
+++ src/js/catalogSearch.js
@@ -4,46 +4,33 @@ import { refs } from './models/refs';
 import { renderError, errorCatalogMarkup } from './errortrailer';
 import createMovieCard from './catalogMovieCard';
 import { dataObj } from './models/data';
+import CreatePagination from './services/pagination';
 
 refs.catalogForm.addEventListener('submit', onSubmit);
-// let page = 1;
 
-export async function onSubmit(event) {
-  event.preventDefault();
-  const form = event.currentTarget;
-  const value = form.elements.searchQuery.value.trim();
-  const country = form.elements.country.value;
-  const year = form.elements.year.value;
-  
+export async function sendSearch(page) {
   try {
-    if (value === '') Notify.failure('No movie specified!');
-    else {
-      
-      // const options = {
-      //   query: value,
-      //   include_adult: false,
-      //   primary_release_year: year,
-      //   page: 1,
-      //   region: country,
-      //   year: year,
-      // };
-      const options = setSearchParam(1, value, year, country);
-      const response = await getSearchMovie(options);
-      console.dir(response);
-      const arrayMovies = await response.results;
-      
+    const options = {
+      query: dataObj.searchQuery,
+      primary_release_year: dataObj.searchYear,
+      page: dataObj.searchCurrentPage,
+      region: dataObj.searchRegion,
+      year: dataObj.searchYear,
+    };
+    const data = await getSearchMovie(options);
+    const arrayMovies = await data.results;
+    // console.dir(arrayMovies);
 
-      if (arrayMovies.length) {
-        catalogListReset();
-        filterCreateMovieCard(response);
-        refs.catalogForm.reset();
-      } else {
-        catalogListReset();
-        renderBtnReset();
-        renderError(refs.catalogList, errorCatalogMarkup);
-      }
-      // renderBtnReset();
+    if (arrayMovies.length) {
+      catalogListReset();
+      filterCreateMovieCard(data);
+      refs.catalogForm.reset();
+    } else {
+      catalogListReset();
+      renderBtnReset();
+      renderError(refs.catalogList, errorCatalogMarkup);
     }
+
     refs.buttonReset.addEventListener('click', e => {
       refs.catalogForm.reset();
       hiddenBtnReset();
@@ -55,33 +42,76 @@ export async function onSubmit(event) {
   }
 }
 
-export function filterCreateMovieCard(response) {
-  screen.width <= 767
-    ? createMovieCard(response, refs.catalogList, 10)
-    : createMovieCard(response, refs.catalogList, 20);
+export async function onSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const value = form.elements.searchQuery.value.trim();
+  const country = form.elements.country.value;
+  const year = form.elements.year.value;
 
-// TODO:  fix pagination functionality
- git
+  if (value === '') Notify.failure('No movie specified!');
+  let page = 1;
+  const options = setSearchParam(page, value, year, country);
+ 
+  sendSearch((page));
+  // TODO:  fix pagination functionality
+  const searchPagination = new CreatePagination(response, sendSearch(page));
+  searchPagination.activatePagination();
+
+  // try {
+  //   if (value === '') Notify.failure('No movie specified!');
+  //   else {
+  //     const options = setSearchParam(page, value, year, country);
+  //     const response = await getSearchMovie(options);
+  //     const arrayMovies = await response.results;
+  //     console.dir(arrayMovies);
+
+  //     if (arrayMovies.length) {
+  //       catalogListReset();
+  //       filterCreateMovieCard(response);
+  //       refs.catalogForm.reset();
+  //     } else {
+  //       catalogListReset();
+  //       renderBtnReset();
+  //       renderError(refs.catalogList, errorCatalogMarkup);
+  //     }
+  //     // renderBtnReset();
+  //   }
+  //   refs.buttonReset.addEventListener('click', e => {
+  //     refs.catalogForm.reset();
+  //     hiddenBtnReset();
+  //   });
+  // } catch (error) {
+  //   console.error(error);
+  //   catalogListReset();
+  //   renderError(refs.catalogList, errorCatalogMarkup);
+  // }
+}
+
+function filterCreateMovieCard(data) {
+  screen.width <= 767
+    ? createMovieCard(data.results, refs.catalogList, 10)
+    : createMovieCard(data.results, refs.catalogList, 20);
 }
 // ==========================================================================================
-export function renderBtnReset() {
+function renderBtnReset() {
   refs.buttonReset.classList.remove('hidden');
   refs.buttonReset.classList.add('active');
   // refs.buttonSearchCatalog.disabled = true;
 }
 // =========================================================================================
-export function hiddenBtnReset() {
+function hiddenBtnReset() {
   refs.buttonReset.classList.remove('active');
   refs.buttonReset.classList.add('hidden');
   // refs.buttonSearchCatalog.disabled = false;
 }
 // ==========================================================================================
-export function catalogListReset() {
+function catalogListReset() {
   refs.catalogList.innerHTML = '';
 }
 // ==========================================================================================
-export function setSearchParam(page, query, year, region) {
-  dataObj.searchCurrentPage = page + 1;
+function setSearchParam(page = 1, query, year, region) {
+  dataObj.searchCurrentPage = page;
   dataObj.searchQuery = query;
   dataObj.searchYear = year;
   dataObj.searchRegion = region;
@@ -94,19 +124,10 @@ export function setSearchParam(page, query, year, region) {
   };
   return options;
 }
-// {
-//   query = 'qqq',
-//   language = 'en-US',
-//   include_adult = false,
-//   primary_release_year = '',
-//   page = 1,
-//   region = '',
-//   year = '',
-// }
 //================================================================
 function clearSearchParam() {
   dataObj.searchCurrentPage = 0;
-  dataObj.searchQuery = "";
-  dataObj.searchYear = "";
-  dataObj.searchRegion = "";
+  dataObj.searchQuery = '';
+  dataObj.searchYear = '';
+  dataObj.searchRegion = '';
 }
--- src/js/models/data.js
+++ src/js/models/data.js
@@ -1,6 +1,7 @@
 export const dataObj = {
   genreMovieList: null,
   configLanguages: null,
+  
   searchCurrentPage: 0,
   searchQuery: "mouse",
   searchYear: "",
