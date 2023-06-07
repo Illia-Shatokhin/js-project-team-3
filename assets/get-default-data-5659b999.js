deb62e10246ada6b7097b8ecf756281227350da9
Illia-Shatokhin <119238020+Illia-Shatokhin@users.noreply.github.com>
2023-06-07 14:00:20 +0000

    Deploying to gh-pages from @ Illia-Shatokhin/js-project-team-3@ab496a52951fe4c178e82bb083161ddd215d6f56 🚀

--- assets/get-default-data-5659b999.js
+++
@@ -1,1 +0,0 @@
-import{f as t,d as e,h as g}from"./header-b51c3725.js";async function n(){try{const a=await t();a.genres&&(e.genreMovieList=a.genres),e.configLanguages=await g()}catch{}}n();
---
+++ assets/get-default-data-96f7efe9.js
@@ -0,0 +1,1 @@
+import{f as t,d as e,h as g}from"./header-46579534.js";async function n(){try{const a=await t();a.genres&&(e.genreMovieList=a.genres),e.configLanguages=await g()}catch{}}n();
