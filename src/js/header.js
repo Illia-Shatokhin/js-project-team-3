7c2bd528e553530e2653bbdce1999eb19e7128ca
VolodymyK <114539981+VolodymyK@users.noreply.github.com>
2023-06-07 13:28:18 -0500

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -55,30 +55,43 @@ if (
     button.classList.remove('dark-theme');
   });
 }
-// Get the current page URL
+  // Get the current page URL
   var currentPage = window.location.href;
 
-  // Check if the current page matches each menu item
+  // Get the navigation menu items
   var homeLink = document.getElementById('home');
   var catalogLink = document.getElementById('catalog');
-  var myLibraryLink = document.getElementById('my-library');
+var myLibraryLink = document.getElementById('my-library');
+  
+  var homeLink1 = document.getElementById('home1');
+  var catalogLink1 = document.getElementById('catalog1');
+  var myLibraryLink1 = document.getElementById('my-library1');
 
+  // Remove the "current-page" class from all links
+  homeLink.classList.remove('current-page');
+  catalogLink.classList.remove('current-page');
+myLibraryLink.classList.remove('current-page');
+
+  homeLink1.classList.remove('current-page');
+  catalogLink1.classList.remove('current-page');
+  myLibraryLink1.classList.remove('current-page');
+
+  // Check if the current page matches each menu item and add the "current-page" class
   if (currentPage.includes('index.html')) {
     homeLink.classList.add('current-page');
   } else if (currentPage.includes('catalog.html')) {
     catalogLink.classList.add('current-page');
   } else if (currentPage.includes('my-library.html')) {
     myLibraryLink.classList.add('current-page');
+  } else {
+    homeLink.classList.add('current-page');
 }
-  
-var homeLink = document.getElementById('home1');
-var catalogLink = document.getElementById('catalog1');
-var myLibraryLink = document.getElementById('my-library1');
-
-if (currentPage.includes('index.html')) {
-  homeLink.classList.add('current-page');
-} else if (currentPage.includes('catalog.html')) {
-  catalogLink.classList.add('current-page');
-} else if (currentPage.includes('my-library.html')) {
-  myLibraryLink.classList.add('current-page');
-}
+  if (currentPage.includes('index.html')) {
+    homeLink1.classList.add('current-page');
+  } else if (currentPage.includes('catalog.html')) {
+    catalogLink1.classList.add('current-page');
+  } else if (currentPage.includes('my-library.html')) {
+    myLibraryLink1.classList.add('current-page');
+  } else {
+    homeLink1.classList.add('current-page');
+  }
