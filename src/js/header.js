c783cd096dabca176bb03f10e344f5ac7eb8c197
Illia Shatokhin <resonane.2106@gmail.com>
2023-06-08 10:59:06 +0300

    repair buttons

--- src/js/header.js
+++ src/js/header.js
@@ -19,7 +19,7 @@ if (document.getElementById('close-menu')) {
 
 // Dark mode theme toggle
 var themeToggle = document.getElementById('theme-toggle');
-var buttons = document.querySelectorAll('button');
+// var buttons = document.querySelectorAll('button');
 
 themeToggle.addEventListener('change', function () {
   if (themeToggle.checked) {
@@ -51,49 +51,49 @@ if (
 } else {
   themeToggle.checked = false;
   document.body.classList.remove('dark-theme');
-  buttons.forEach(function (button) {
-    button.classList.remove('dark-theme');
-  });
+  // buttons.forEach(function (button) {
+  //   button.classList.remove('dark-theme');
+  // });
 }
-  // Get the current page URL
-  var currentPage = window.location.href;
+// Get the current page URL
+var currentPage = window.location.href;
 
-  // Get the navigation menu items
-  var homeLink = document.getElementById('home');
-  var catalogLink = document.getElementById('catalog');
+// Get the navigation menu items
+var homeLink = document.getElementById('home');
+var catalogLink = document.getElementById('catalog');
 var myLibraryLink = document.getElementById('my-library');
-  
-  var homeLink1 = document.getElementById('home1');
-  var catalogLink1 = document.getElementById('catalog1');
-  var myLibraryLink1 = document.getElementById('my-library1');
 
-  // Remove the "current-page" class from all links
-  homeLink.classList.remove('current-page');
-  catalogLink.classList.remove('current-page');
+var homeLink1 = document.getElementById('home1');
+var catalogLink1 = document.getElementById('catalog1');
+var myLibraryLink1 = document.getElementById('my-library1');
+
+// Remove the "current-page" class from all links
+homeLink.classList.remove('current-page');
+catalogLink.classList.remove('current-page');
 myLibraryLink.classList.remove('current-page');
 
-  homeLink1.classList.remove('current-page');
-  catalogLink1.classList.remove('current-page');
-  myLibraryLink1.classList.remove('current-page');
+homeLink1.classList.remove('current-page');
+catalogLink1.classList.remove('current-page');
+myLibraryLink1.classList.remove('current-page');
 
-  // Check if the current page matches each menu item and add the "current-page" class
-  if (currentPage.includes('index.html')) {
-    homeLink.classList.add('current-page');
-  } else if (currentPage.includes('catalog.html')) {
-    catalogLink.classList.add('current-page');
-  } else if (currentPage.includes('my-library.html')) {
-    myLibraryLink.classList.add('current-page');
-  } else {
-    homeLink.classList.add('current-page');
+// Check if the current page matches each menu item and add the "current-page" class
+if (currentPage.includes('index.html')) {
+  homeLink.classList.add('current-page');
+} else if (currentPage.includes('catalog.html')) {
+  catalogLink.classList.add('current-page');
+} else if (currentPage.includes('my-library.html')) {
+  myLibraryLink.classList.add('current-page');
+} else {
+  homeLink.classList.add('current-page');
 }
-  if (currentPage.includes('index.html')) {
-    homeLink1.classList.add('current-page');
-  } else if (currentPage.includes('catalog.html')) {
-    catalogLink1.classList.add('current-page');
-  } else if (currentPage.includes('my-library.html')) {
-    myLibraryLink1.classList.add('current-page');
-  } else {
-    homeLink1.classList.add('current-page');
+if (currentPage.includes('index.html')) {
+  homeLink1.classList.add('current-page');
+} else if (currentPage.includes('catalog.html')) {
+  catalogLink1.classList.add('current-page');
+} else if (currentPage.includes('my-library.html')) {
+  myLibraryLink1.classList.add('current-page');
+} else {
+  homeLink1.classList.add('current-page');
 }
 // Mobile menu toggle
 var menuToggle = document.getElementById('menu-toggle');
@@ -138,11 +138,3 @@ document.getElementById('close-menu').addEventListener('click', function () {
     mobileMenu.classList.remove('visible');
   }, 300);
 });
-
-
-
-
-
-
-
-
